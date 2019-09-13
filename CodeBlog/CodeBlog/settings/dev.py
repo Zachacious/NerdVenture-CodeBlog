ef4a1e716a0f44f67e65059d1332d1a924770729
Zachacious <back4moore@gmail.com>
2019-09-13 18:55:19 -0500

    udate

--- CodeBlog/CodeBlog/settings/dev.py
+++ CodeBlog/CodeBlog/settings/dev.py
@@ -26,4 +26,4 @@ NEWSLETTER_FROM_EMAIL = 'Zach@Zach.com'
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'localhost.com:8000'
+BASE_URL = 'localhost:8000'
