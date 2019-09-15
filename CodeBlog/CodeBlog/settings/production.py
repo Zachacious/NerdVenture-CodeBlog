47fbf10826f3c4d618d2be414550552026617e28
Zachacious <back4moore@gmail.com>
2019-09-14 19:53:30 -0500

    update schema

--- CodeBlog/CodeBlog/settings/production.py
+++ CodeBlog/CodeBlog/settings/production.py
@@ -2,6 +2,10 @@ from .base import *
 from .pipelineConf_Prod import *
 import os
 
+from dotenv import load_dotenv
+# project_folder = os.path.expanduser('~/NerdVenture-CodeBlog')  # adjust as appropriate
+load_dotenv(os.path.join(PROJECT_DIR, '.env'))
+
 DEBUG = False
 
 MESSAGE_LEVEL = 20
@@ -23,19 +27,25 @@ INSTALLED_APPS = apps + INSTALLED_APPS
 
 WAGTAIL_CACHE = True
 
-NEWSLETTER_FROM_EMAIL = 'Zach@Zach.com'
+NEWSLETTER_FROM_EMAIL = 'Zach@NerdVenture.Net'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_MESSAGES_RECEIVER = 'zach@site.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_MESSAGES_RECEIVER = 'zach@NerdVenture.Net'
+EMAIL_SUBJECT_PREFIX = "[NerdVenture] "
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# WAGTAILFRONTENDCACHE = {
-#     'cloudflare': {
-#         'BACKEND': 'wagtail.contrib.frontend_cache.backends.CloudflareBackend',
-#         'EMAIL': 'your-cloudflare-email-address@example.com',
-#         'TOKEN': 'your cloudflare api token',
-#         'ZONEID': 'your cloudflare domain zone id',
-#     },
-# }
+WAGTAILFRONTENDCACHE = {
+    'cloudflare': {
+        'BACKEND': 'wagtail.contrib.frontend_cache.backends.CloudflareBackend',
+        'EMAIL': 'back4moore@gmail.com',
+        'TOKEN': 'gCpbxD4ii8u-1QLdSO36dJtHesBmbz0t8KhVA622',
+        'ZONEID': 'f3420199adb7a885e8a5cf55654e1a12',
+    },
+}
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
