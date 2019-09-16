34b0d2fa694ea594ab7133bbcdfc540e091eb5fc
Zachacious <back4moore@gmail.com>
2019-09-16 17:45:54 -0500

    update static file finders

--- CodeBlog/CodeBlog/settings/base.py
+++ CodeBlog/CodeBlog/settings/base.py
@@ -194,8 +194,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_FINDERS = [
-    # 'django.contrib.staticfiles.finders.FileSystemFinder',
-    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'pipeline.finders.CachedFileFinder',
     'pipeline.finders.PipelineFinder',
     
