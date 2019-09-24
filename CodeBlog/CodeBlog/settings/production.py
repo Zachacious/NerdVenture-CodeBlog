e500b5b9f9591ada22d5ccce6b29a9c3a18c9710
Zachacious <back4moore@gmail.com>
2019-09-23 22:03:06 -0500

    update

--- CodeBlog/CodeBlog/settings/production.py
+++ CodeBlog/CodeBlog/settings/production.py
@@ -57,8 +57,8 @@ BASE_URL = 'https://www.nerdventure.com'
 
 ALLOWED_HOSTS = ['www.nerdventure.net', 'nerdventure.net'] 
 
-# STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
-STATICFILES_STORAGE = 'CodeBlog.storage.GZIPStorage'
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+# STATICFILES_STORAGE = 'CodeBlog.storage.GZIPStorage'
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
