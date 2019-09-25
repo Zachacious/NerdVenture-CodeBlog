954f7176ff3199da76fc8b1232d675f638373786
Zachacious <back4moore@gmail.com>
2019-09-25 17:32:14 -0500

    update filenames for pipeline

--- CodeBlog/CodeBlog/settings/pipelineConf_Prod.py
+++ CodeBlog/CodeBlog/settings/pipelineConf_Prod.py
@@ -14,7 +14,7 @@ PIPELINE = {
                 'social/js/social.js',
                 'js/progressive-image.js',
             ),
-            'output_filename': 'js/general.min.js',
+            'output_filename': 'js/general.js',
             'extra_context': {
                 # 'async': True,
                 'defer': True,
@@ -33,7 +33,7 @@ PIPELINE = {
                 'css/header.css',
                 'css/anims.css',
             ),
-            'output_filename': 'css/general.min.css',
+            'output_filename': 'css/general.css',
         },
     },
 }
