7732263b65744eecbd80e4ce496032259c4cbe98
Zachacious <back4moore@gmail.com>
2019-09-25 18:46:13 -0500

    update

--- CodeBlog/CodeBlog/settings/base.py
+++ CodeBlog/CodeBlog/settings/base.py
@@ -186,7 +186,7 @@ USE_TZ = True
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # 'pipeline.finders.PipelineFinder',
+    'pipeline.finders.PipelineFinder',
     'pipeline.finders.CachedFileFinder',
     # 'CodeBlog.finders.LeftoverPipelineFinder',
 ]
