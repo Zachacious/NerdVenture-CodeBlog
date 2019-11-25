ce6be8b5c1eb77b6c995ca5d64229f8d70e6cdfb
Zachacious <back4moore@gmail.com>
2019-11-24 22:06:48 -0600

    fix production manage.py to use the right db

--- CodeBlog/manage.py
+++ CodeBlog/manage.py
@@ -2,8 +2,16 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/NerdVenture-CodeBlog')
+load_dotenv(os.path.join(project_folder, '.env'))
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CodeBlog.settings.dev")
+    
+    if os.getenv('IS_PRODUCTION')=='True':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CodeBlog.settings.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CodeBlog.settings.dev")
 
     from django.core.management import execute_from_command_line
 
